6cfd1e2f17f799120bfc99d476ad1d24b62da5b3
tm-pham <thi.mui.pham@posteo.de>
2025-08-22 16:25:01 -0400

    More debugging [prints in infect_distribution

--- R/infect_distribution.R
+++ R/infect_distribution.R
@@ -32,8 +32,9 @@ infect_distribution <- function(time, inftimes, le,
     
     # To compute the CDF at value x (e.g. t days after infection):
     prob_neg <- prod(1 - pgamma(lastneg.time - time, shape=shape, scale=scale))
-    if(is.na(prob_neg)){
+    if(any(is.na(prob_neg))){
       cat("Warning: Probability of not being infected before last negative time is NA.\n")
+      cat("prob_neg = ", prob_neg, "\n")
       cat("lastneg.time = ", lastneg.time, "\n")
       cat("shape = ", shape, "\n")
       cat("scale = ", scale, "\n")
@@ -67,8 +68,9 @@ infect_distribution <- function(time, inftimes, le,
     #print(list(time, inftimes, le, nodetimes, host, log))
     # inf_func <- le$p$inf_function
     prob <- le$p$inf_function(time, inftimes, le, nodetimes, host, log)
-    if(is.na(prob)){
+    if(any(is.na(prob))){
       cat("Warning: Probability of infection is NA.\n")
+      cat("prob = ", prob, "\n")
     }
     prob <- prob*prob_neg # If last-negative time is given, multiply with the probability of not being infected before that time
     return(prob)
