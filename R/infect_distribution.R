05d44b55ac2bcbfe48164b62beaece8c0938515d
tm-pham <thi.mui.pham@posteo.de>
2025-08-22 16:14:07 -0400

    Debugging mcmc-update-host-paths

--- R/infect_distribution.R
+++ R/infect_distribution.R
@@ -32,6 +32,12 @@ infect_distribution <- function(time, inftimes, le,
     
     # To compute the CDF at value x (e.g. t days after infection):
     prob_neg <- prod(1 - pgamma(lastneg.time - time, shape=shape, scale=scale))
+    if(is.na(prob_neg)){
+      cat("Warning: Probability of not being infected before last negative time is NA.\n")
+      cat("lastneg.time = ", lastneg.time, "\n")
+      cat("shape = ", shape, "\n")
+      cat("scale = ", scale, "\n")
+    }
   }else{
     prob_neg <- 1
   }
@@ -61,6 +67,9 @@ infect_distribution <- function(time, inftimes, le,
     #print(list(time, inftimes, le, nodetimes, host, log))
     # inf_func <- le$p$inf_function
     prob <- le$p$inf_function(time, inftimes, le, nodetimes, host, log)
+    if(is.na(prob)){
+      cat("Warning: Probability of infection is NA.\n")
+    }
     prob <- prob*prob_neg # If last-negative time is given, multiply with the probability of not being infected before that time
     return(prob)
 
